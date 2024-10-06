92fd397172f5556525f83b3e6dd255c52cfe88d1
Rodger Milla Dev <rodgermilladev@gmail.com>
2024-10-07 01:50:42 +0300

    New Update

---
+++ public/JS/config.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyBwFxFPeSQ3pnVozFw92gaKiCXin0jajTw",
+    authDomain: "rodger-codes.firebaseapp.com",
+    projectId: "rodger-codes",
+    storageBucket: "rodger-codes.appspot.com",
+    messagingSenderId: "998872040420",
+    appId: "1:998872040420:web:66470426ad28e31884bdf1",
+    measurementId: "G-6MDT42NZ0Q"
+  };
+  firebase.initializeApp(firebaseConfig);
